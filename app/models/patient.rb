128804983b4fe56fbea47856135489e7258edbca
Farhana <farhanamitu.cse.ru@gmail.com>
2017-07-23 23:52:25 +0600

    create test.sqlite33 for unit testing

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,2 @@
 class Patient < ApplicationRecord
-	validates :name, presence: true
-	validates :age, presence: true, :numericality => true
-	validates :weight, presence: true, :numericality => true
-	validates :disease, presence: true
 end
