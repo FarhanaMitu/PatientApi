bda2e5be92c9fc7c96b98127429c2f18340b2e3e
Farhana <farhanamitu.cse.ru@gmail.com>
2017-07-23 22:23:43 +0600

    create admin for token authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723160111) do
+ActiveRecord::Schema.define(version: 20170723162230) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "name"
